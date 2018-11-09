e3508f0502ca6e447395307730795e62d72146a1
Israel Cyabukombe <israelcyabu93@gmail.com>
2018-11-09 14:02:26 -0600

    add title to cyabukoi.h
    add title to cyabukoi.h

--- cyabukoi5/cyabukoi5.h
+++ cyabukoi5/cyabukoi5.h
@@ -1,5 +1,5 @@
 /********************************************************************
-*** NAME : Israel Cyabukombe
+*** NAME : Israel Cyabukombe Software Engineer
 *** CLASS : CSc 346
 *** ASSIGNMENT : 	5
 *** DUE DATE : 4/13/2016
